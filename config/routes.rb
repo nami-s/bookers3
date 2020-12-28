3f9fac9aa784da1227f1b50e2503a5f7c647617d
nami-s <na.s1222.10feettt@gmail.com>
2020-12-28 11:17:27 +0000

    [Add]bookcommentsコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  
+
   root 'homes#top'
   get 'home/about' => 'homes#about'
 
-  resources :books
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+  end
+
   devise_for :users
   resources :users, only: [:show,:index,:edit,:update]
 
