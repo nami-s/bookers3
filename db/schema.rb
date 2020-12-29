40139c541d806951456cd03c4be213b973391a97
nami-s <na.s1222.10feettt@gmail.com>
2020-12-29 09:24:11 +0000

    [Add]コメント機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_022851) do
+ActiveRecord::Schema.define(version: 2020_12_29_065803) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
